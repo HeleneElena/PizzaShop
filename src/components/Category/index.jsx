42e6cc064719561e671c3a62ddb32d5657d16f2f
HeleneElena <heleneebert1@gmail.com>
2022-10-27 18:37:12 +0200

    feat: Category

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
 
 
 export const Category = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const LIST = ['Все','Мясные','Вегетарианская','Гриль','Острые', 'Закрытые'];
+
+    const changeActive = (index) => {
+        setActiveIndex(index);
+    };
+
     return (
         <div className="categories">
             <ul>
-            <li className="active">Все</li>
-            <li>Мясные</li>
-            <li>Вегетарианская</li>
-            <li>Гриль</li>
-            <li>Острые</li>
-            <li>Закрытые</li>
+                {
+                    LIST.map((category, index) => {
+                        return <li key={index} onClick={() => changeActive(index)} className={activeIndex === index ? 'active' : ''}>{category}</li>
+                    })
+                }
+            
             </ul>
         </div>
     );

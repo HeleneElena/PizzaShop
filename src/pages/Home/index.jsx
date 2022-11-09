a38dd3f8afa2d7b426bd8f4dc10625b2118faa39
HeleneElena <heleneebert1@gmail.com>
2022-11-09 19:58:04 +0100

    fix: adaptiv

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -20,19 +20,19 @@ export const Home = () => {
     return (
         <div className="content">
             <div className="container">
-            <div className="content__top">
-                <Category pizzas={items} />
-                <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-            {
-                isLoading ? [...new Array(5)].map((_, index) => <Skeleton key={index} />) : (
-                items.map(obj => <PizzaBlock key={obj.id} {...obj} />)
-                )
-            }
-        
-            </div>
+                <div className="content__top">
+                    <Category pizzas={items} />
+                    <Sort />
+                </div>
+                <h2 className="content__title">Все пиццы</h2>
+                <div className="content__items">
+                {
+                    isLoading ? [...new Array(5)].map((_, index) => <Skeleton key={index} />) : (
+                    items.map(obj => <PizzaBlock key={obj.id} {...obj} />)
+                    )
+                }
+            
+                </div>
             </div>
         </div>
     );

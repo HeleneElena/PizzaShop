e8c411788f7e9fe07c6e4599b59901c9e0a03f06
EbertHelene <91485528+HeleneElena@users.noreply.github.com>
2023-01-24 19:31:09 +0100

    Add files via upload

--- aufgaben.js
+++ aufgaben.js
@@ -1,63 +1,91 @@
-// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
-function addLength(str) {
-  let arr = str.split(' ');
-  let result = arr.map(el => el + ` ${el.length}`);
-  return result;
+// Write a function that takes a positive integer n, sums all the cubed 
+// values from 1 to n, and returns that sum.
+//Assume that the input n will always be a positive integer.
+function sumCubes(n){
+  let sum = 0;
+  for (let i = 1; i <=n; i++) {
+    sum += i**3;
+  }
+  return sum;
 }
 
-// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
-function repeatStr (n, s) {
-  return s.repeat(n);
-}
-
-// Nathan loves cycling.Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
-// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
-function litres(time) {
-  return parseInt(time * 0.5);
-}
-
-// Your task is to make two functions ( max and min, or maximum and minimum, etc., 
-// depending on the language ) that receive a list of integers as input, and 
-// return the largest and lowest number in that list, respectively.
-const min = function(list) {
-    return Math.min(...list);
+// A square of squares
+//You like building blocks. You especially like building blocks that are squares. 
+//And what you even like more, is to arrange them into a square of square building blocks!
+//However, sometimes, you can't arrange them into a square. Instead, you end up with
+// an ordinary rectangle! Those blasted things! If you just had a way to know, whether 
+// you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+let isSquare = function(n){
+  let x = Math.sqrt(n);
+  return x * x === n;
 };
 
-const max = function(list) {
-    return Math.max(...list);
-};
-
-// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
-function shortcut (string) {
-  return string.replace(/[aeiou]+/g, "");
-}
-
-// Write a function named setAlarm which receives two parameters. The first parameter, employed, 
-//is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
-// The function should return true if you are employed and not on vacation (because these are 
-// the circumstances under which you need to set an alarm). It should return false otherwise.
-function setAlarm(employed, vacation) {
-    return (employed && !vacation) ? true : false;
-}
-
-// Grade book Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
-function getGrade (s1, s2, s3) {
-  let score = (s1 + s2 + s3) / 3;
-    if (score >= 90 && score <= 100) {
-        return 'A';
-    } else if (score >= 80 && score < 90) {
-        return 'B';
-    } else if (score >= 70 && score < 80) {
-        return 'C';
-    } else if (score >= 60  && score < 70) {
-        return 'D';
-    } else {
-        return 'F';
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+// Division by zero should return NaN.
+function remainder(n, m){
+if (n > m) {
+    let answer = n % m;
+    if (m === 0) {
+      return NaN;
     }
+    else {
+      return answer;
+    }
+  }
+  else if (m > n) {
+    let answer = m % n;
+    if (n === 0) {
+      return NaN;
+    }
+    else {
+      return answer;
+    }
+  }
+  else {
+    let answer = n % m;
+    return answer;
+  }
 }
 
-// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. 
-// For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
-function remove (str) {
-   return str.split('!').join('')+'!';
+// Given an array of integers.
+//Return an array, where the first element is the count of positives numbers 
+// and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+function countPositivesSumNegatives(input) {
+   if (input && input.length) {
+        let posSum = 0;
+        let negSum = 0;
+        input.forEach(num => num > 0 ? posSum++ : negSum += num);
+        return [posSum, negSum];
+    } else {
+        return [];
+    }
+  }
+
+// Character recognition software is widely used to digitise printed texts. 
+// Thus the texts can be edited, searched and stored on a computer.
+// When documents (especially pretty old ones written with a typewriter), 
+//are digitised character recognition softwares often make mistakes.
+  function correct(s) {
+    let s1 = s.replace(/5/g, 'S');
+    let s2 = s1.replace(/0/g, 'O');
+    let s3 = s2.replace(/1/g, 'I');
+    return s3;
+}
+
+// There are pillars near the road. 
+// The distance between the pillars is the same and the width of the pillars is the same. Your function accepts three arguments:
+// number of pillars (≥ 1);
+// distance between pillars (10 - 30 meters);
+// width of the pillar (10 - 50 centimeters).
+// Calculate the distance between the first and the last pillar in centimeters
+// (without the width of the first and last pillar).
+function pillars(numPill, dist, width) {
+  if (numPill == 1) {
+    return 0;
+  } else if (numPill == 2) {
+    return dist * 100;
+  } else {
+    return ((dist * 100)* (numPill-1)) + (width * (numPill - 2));
+  }
 }

fb5270a8cea764c33375aa4aacb6b23a5a9cadec
EbertHelene <91485528+HeleneElena@users.noreply.github.com>
2023-02-16 12:11:29 +0100

    commit18

--- елки/елки/script/modal.js
+++ елки/елки/script/modal.js
@@ -1,16 +1,20 @@
-const modalClose = document.querySelector('.modal__close'),
-      overlayOrder = document.querySelector('.overlay_order'),
-      buttonOrder = document.querySelectorAll('.button__order'),
+const overlayOrder = document.querySelector('.overlay_order'),
+      buttonsOrder = document.querySelectorAll('.button__order'),
       modal = document.querySelector('.modal');
 
-buttonOrder.forEach(el => {
+buttonsOrder.forEach(el => {
     el.addEventListener('click', () => {
         overlayOrder.classList.add('overlay_active');
         modal.style.display = 'block';
+        const order = overlayOrder.querySelector('.modal__order');
+        order.value = el.dataset.order;
     });
 });
 
-modalClose.addEventListener('click', () => {
-    overlayOrder.classList.remove('overlay_active');
-    modal.style.display = 'none';
+overlayOrder.addEventListener('click', (e) => {
+    if (e.target === overlayOrder || e.target.closest('.modal__close')) {
+        overlayOrder.classList.remove('overlay_active');
+        modal.style.display = 'none';
+    }
 });
+
